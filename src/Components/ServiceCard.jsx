f296f95191e7a92cc45583a3ae8820e502ccdf83
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-09-17 14:43:35 +0600

    service section designed

---
+++ src/Components/ServiceCard.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+function ServiceCard({ imgURL, label, subtext }) {
+  return (
+    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
+      <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-full">
+        <img src={imgURL} alt={label} width={24} height={24} />
+      </div>
+      <h2 className="mt-3 text-2xl font-palanquin font-bold leading-normal">{label}</h2>
+      <p className="mt-3 text-xl break-words font-montserrat leading-normal text-slate-gray">{subtext}</p>
+    </div>
+  );
+}
+ServiceCard.propTypes = {
+    label: PropTypes.string,
+    imgURL: PropTypes.string,
+    subtext: PropTypes.string,
+  };
+export default ServiceCard;
