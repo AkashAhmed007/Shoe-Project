cce00e72e8ab35c38e9fb4973d5e8f491326e180
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-09-17 20:43:28 +0600

    subscribe section designed

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
-function Button({label,iconURL,backgroundColor,textColor,borderColor}) {
+function Button({label,iconURL,backgroundColor,textColor,borderColor,fullWidth}) {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border leading-none text-lg
       ${
         backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}`: "text-white bg-coral-red border-coral-red"
 
       }
-     rounded-full font-montserrat`}>
+     rounded-full ${fullWidth && 'w-full'}font-montserrat`}>
         {label}
         {iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />}
     </button>
@@ -17,6 +17,7 @@ Button.propTypes = {
     iconURL:PropTypes.string,
     backgroundColor:PropTypes.string,
     textColor:PropTypes.string,
-    borderColor:PropTypes.string
+    borderColor:PropTypes.string,
+    fullWidth:PropTypes.string,
   };
 export default Button
